46892aae2d7c414ac585288624c4fb5371d4b11d
reiji11011 <reiji.sugama@gmail.com>
2023-04-19 22:27:56 +0900

    [add] DB接続処理を追加する。

--- internal/app/infrastructures/database/database.go
+++ internal/app/infrastructures/database/database.go
@@ -3,16 +3,46 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"os"
+	"time"
 )
 
 func ConnectDb() {
 	fmt.Println("DBへ接続を行います")
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go_ca?parseTime=true")
+
+	jst, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
 		panic(err.Error())
 	}
-	defer db.Close()
+
+	// 環境変数から取得
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	addr := os.Getenv("MYSQL_ADDRESS")
+	dbName := os.Getenv("MYSQL_DB_NAME")
+
+	config := mysql.Config{
+		User:      user,
+		Passwd:    pass,
+		Addr:      addr,
+		DBName:    dbName,
+		Loc:       jst,
+		ParseTime: true,
+	}
+
+	db, err := sql.Open("mysql", config.FormatDSN())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+	}(db)
 
 	err = db.Ping()
 	if err != nil {
