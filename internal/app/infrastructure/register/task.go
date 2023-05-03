4e204f46b3609e8cd22f90db9951af6c15c0ba68
reiji11011 <reiji.sugama@gmail.com>
2023-05-03 16:49:35 +0900

    [add] domain層のルールを追記する。

--- internal/app/infrastructure/register/task.go
+++ internal/app/infrastructure/register/task.go
@@ -1,16 +1,16 @@
 package register
 
 import (
-	taskController "go-ca/internal/app/adaptor/controller/task"
-	taskUsecase "go-ca/internal/app/application/usecase/task"
-	taskRepository "go-ca/internal/app/infrastructure/repository/task"
+	tasksController "go-ca/internal/app/adaptor/controller/task"
+	tasksUsecase "go-ca/internal/app/application/usecase/task"
+	tasksRepository "go-ca/internal/app/infrastructure/repository/task"
 	"net/http"
 )
 
 func Tasks(w http.ResponseWriter, r *http.Request) {
 	// DI
-	tasksRepository := taskRepository.NewTasksRepository()
-	GetTasksUsecase := taskUsecase.NewGetTasksUsecase(&tasksRepository)
-	tasksController := taskController.NewTasksController(&GetTasksUsecase)
+	tasksRepository := tasksRepository.NewTasksRepository()
+	tasksUsecase := tasksUsecase.NewGetTasksUsecase(&tasksRepository)
+	tasksController := tasksController.NewTasksController(&tasksUsecase)
 	tasksController.GetTasks(w, r)
 }
