1420ae17c311490cdc4e0b26bd9c858884759185
reiji11011 <reiji.sugama@gmail.com>
2023-05-14 13:34:37 +0900

    [add] 実態は直接呼び出せてしまう場合を追加する。

--- internal/app/domain/task/task.go
+++ internal/app/domain/task/task.go
@@ -20,12 +20,14 @@ func NewTask(taskId uint64, name string, reward Reward) (*Task, error) {
 	// TODO 以下を呼べなくする
 	// reward = RewardImpl{value: 100} syntax error
 	//reward2 := Reward{} Invalid composite literal type: Reward
-	fmt.Println(reward)
+	// しかし、rewardImplを直接呼び出せて、rewardとして返せてしまう
+	a := rewardImpl{value: 100}
+	fmt.Println(a)
 
 	return &Task{
 		taskId: taskId,
 		name:   name,
-		reward: reward,
+		reward: a,
 	}, nil
 }
 
